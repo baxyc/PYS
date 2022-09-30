3b6c1649450c058b084001bd80ebd55b2b56a990
Ivan <baxyc.da@gmail.com>
2022-10-01 09:51:49 +1100

    2_3HW Вроде так)

--- 2LS/HW/2_5HW.py
+++ 2LS/HW/2_5HW.py
@@ -3,19 +3,17 @@
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
 
-from random import Random, randint
-N = int(input('Введите число '))
-numbers = []
-for i in range(N):
-    numbers.append(randint(-N,N+1))
-print(numbers)
-def smes(numbers):
-    list = numbers[:]
-    numbers_length = len(list)
-    for i in range(numbers_length):
-        index = randint(0, numbers_length - 1)
-        temp = list[i]
-        list[i] = list[index]
-        list[index] = temp
-    return list
-print(smes(numbers))
+from random import randrange
+
+num = int(input())
+nums_list = list(range(num))
+len_list = len(nums_list)
+
+print(nums_list)
+
+for i in range(len_list):
+    n_1 = randrange(len_list)
+    n_2 = randrange(len_list)
+    nums_list[n_1], nums_list[n_2] = nums_list[n_2], nums_list[n_1]
+
+print(nums_list)
