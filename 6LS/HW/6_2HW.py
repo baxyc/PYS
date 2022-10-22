531a16b9dd7492cba6c85951f9406599ac2c32ad
Ivan <baxyc.da@gmail.com>
2022-10-23 08:29:53 +1100

    6_2HW

--- 6LS/HW/6_2HW.py
+++ 6LS/HW/6_2HW.py
@@ -2,7 +2,7 @@
 # in
 # 100
 # out
-# [20, 21, 40, 42, 60, 63, 80, 84, 100]
+# [20, 21, 40, 42, 60, 63, 80, 84, 100].
 
 def uniq_list(num):
     return [el for el in range(20, num + 1) if el % 20 == 0 or el % 21 == 0]
