5860766057e1afb10f2621b94071c4cacfd62e5d
Ivan <baxyc.da@gmail.com>
2022-11-11 22:07:04 +1100

    bot download YT1

--- 9LS/HW/dowloader.py
+++ 9LS/HW/dowloader.py
@@ -8,5 +8,5 @@ async def download(link, message, bot):
     video_props = video.streams.filter(progressive=True, file_extension='mp4')
     video_props.get_highest_resolution().download(f'{message.chat.id}', f'{message.chat.id}_{video.title}')
     with open(f'{message.chat.id}/{message.chat.id}_{video.title}', 'rb') as file:
-        await bot.send_video(message.chat.id, file, caption="That's your video")
+        await bot.send_video(message.chat.id, file, caption="это твое видео")
         os.remove(f'{message.chat.id}/{message.chat.id}_{video.title}')
